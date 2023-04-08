7713c5bd6353d8a9e7e37697d9afc3e18f393ec5
Bronson Horne <bronsonhorne@gmail.com>
2023-04-07 23:51:47 -0600

    Add project files.

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoviesAPI.Model;
+
+namespace MoviesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController : Controller
+    {
+        private MovieDbContext context;
+        public MovieController(MovieDbContext temp) {
+            context = temp;
+
+        }
+        public IEnumerable<Movies> Get()
+        {
+            var x = context.Movies.ToArray();
+
+            return x;
+        }
+    }
+}
---
+++ Model/MovieDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MoviesAPI.Model
+{
+    public class MovieDbContext :DbContext
+    {
+        public MovieDbContext (DbContextOptions<MovieDbContext> options): base(options) { }
+
+        public DbSet<Movies> Movies { get; set; }
+    }
+}
